b91ae0fab407067a3ec3ddf95db285a1d42c5726
Nando <102630615+Nandoaltz@users.noreply.github.com>
2025-06-22 19:57:08 -0300

    Update lspconfig.lua

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -1,47 +1,52 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        { "folke/neodev.nvim", opts = {} },
-    },
-    config = function()
-        local nvim_lsp = require("lspconfig")
-        local mason_lspconfig = require("mason-lspconfig")
-        local protocol = require("vim.lsp.protocol")
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    { "folke/neodev.nvim", opts = {} },
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls" },  -- Apenas Lua agora
+      automatic_installation = true,
+    })
 
-        local on_attach = function(client, bufnr)
-            if client.server_capabilities.documentFormattingProvider then
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = vim.api.nvim_create_augroup("Format", { clear = true }),
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format()
-                    end,
-                })
-            end
-        end
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local on_attach = function(client, bufnr)
+      local opts = { buffer = bufnr, silent = true }
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
 
-        mason_lspconfig.setup_handlers({
-            function(server)
-                nvim_lsp[server].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-
-            -- Pode colocar servidores com configurações específicas também aqui
-            ["gopls"] = function()
-                nvim_lsp["gopls"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-            -- etc
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+          buffer = bufnr,
+          callback = function() vim.lsp.buf.format() end,
         })
-    end,
+      end
+    end
+
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+    -- Configuração apenas para lua_ls
+    lspconfig.lua_ls.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          runtime = { version = "LuaJIT" },
+          diagnostics = { globals = { "vim" } },
+          workspace = { checkThirdParty = false },
+          telemetry = { enable = false },
+        }
+      }
+    })
+  end
 }
